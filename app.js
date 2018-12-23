6dd9638d737177e9f96f4e5cd718e92bb8aff1cf
Kelvin Modesto <km.modesto@gmail.com>
2018-12-23 00:48:24 -0300

    Delete users.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -14,9 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
